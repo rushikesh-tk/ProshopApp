e4225e59f771ff16caa17f771f4c100b8e9e5a7f
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-11-30 21:22:34 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
